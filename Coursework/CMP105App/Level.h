bd0249e40f906f524e5b9ef7a3ee2a2e8e506e33
Arden Hunt <256573410+arh-garden@users.noreply.github.com>
2026-01-29 14:53:08 +0000

    implement m_snake movement into Player.h

--- Coursework/CMP105App/Level.h
+++ Coursework/CMP105App/Level.h
@@ -18,16 +18,11 @@ private:
 	// Default functions for rendering to the screen.
 
 	// Default variables for level class.
-	enum class Direction{ UP, DOWN, LEFT, RIGHT, UP_RIGHT, DOWN_RIGHT, DOWN_LEFT, UP_LEFT, NONE };
-	Direction m_direction = Direction::NONE;
-	sf::CircleShape m_snake;
-	float m_speed = 300.0f;
-	float m_inputBuffer = 0.f;
+	
+	//sf::CircleShape m_snake;
+
 	bool m_gameOver = false;
 
-	const float INPUT_BUFFER_LENGTH = 0.1f;
-	const float APPROX_ONE_OVER_ROOT_TWO = 0.70710678f;	// 1 / sqrt(2)
-
-	Player m_sheep;
+	Player m_sheep = {&m_input};
 	sf::Texture m_tex_sheep;
 };
--- Coursework/CMP105App/Player.h
+++ Coursework/CMP105App/Player.h
@@ -1,18 +1,27 @@
 #pragma once
 #include "Framework/GameObject.h"
 #include "Framework/Animation.h"
+#include "Framework/Input.h"
 
 class Player : public GameObject
 {
 private:
+	// animation
 	Animation m_walkDown;
 	Animation* m_currentAnimation;
+	
+	// input
+	enum class Direction { UP, DOWN, LEFT, RIGHT, UP_RIGHT, DOWN_RIGHT, DOWN_LEFT, UP_LEFT, NONE };
+	Direction m_direction = Direction::NONE;
 
-	const float INPUT_BUFFER_LENGTH = 0.1f;
+	float m_speed = 300.0f;
+	float m_inputBuffer = 0.f;
+
+	const float INPUT_BUFFER_LENGTH = 0.2f;
 	const float APPROX_ONE_OVER_ROOT_TWO = 0.70710678f;	// 1 / sqrt(2)
 
 public:
-	Player();
+	Player(Input* inputRef);
 
 	void update(float dt);
 	void handleInput(float dt);
